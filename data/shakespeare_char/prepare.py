6a875ed1bfd47a19173a7d5ad6e991ee7eacf647
Huiqiang_Liang <73164875+lianghuiqiang9@users.noreply.github.com>
2024-04-08 11:51:17 +0930

    Update prepare.py

--- data/shakespeare_char/prepare.py
+++ data/shakespeare_char/prepare.py
@@ -11,10 +11,10 @@ import numpy as np
 
 # download the tiny shakespeare dataset
 input_file_path = os.path.join(os.path.dirname(__file__), 'input.txt')
-if not os.path.exists(input_file_path):
-    data_url = 'https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt'
-    with open(input_file_path, 'w') as f:
-        f.write(requests.get(data_url).text)
+#if not os.path.exists(input_file_path):
+#    data_url = 'https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt'
+#    with open(input_file_path, 'w') as f:
+#        f.write(requests.get(data_url).text)
 
 with open(input_file_path, 'r') as f:
     data = f.read()
@@ -22,12 +22,14 @@ print(f"length of dataset in characters: {len(data):,}")
 
 # get all the unique characters that occur in this text
 chars = sorted(list(set(data)))
+print("chars : ",chars)
 vocab_size = len(chars)
 print("all the unique characters:", ''.join(chars))
 print(f"vocab size: {vocab_size:,}")
 
 # create a mapping from characters to integers
-stoi = { ch:i for i,ch in enumerate(chars) }
+stoi = { ch:i for i,ch in enumerate(chars) }# str to int
+print("stoi : ",stoi)
 itos = { i:ch for i,ch in enumerate(chars) }
 def encode(s):
     return [stoi[c] for c in s] # encoder: take a string, output a list of integers
@@ -36,7 +38,8 @@ def decode(l):
 
 # create the train and test splits
 n = len(data)
-train_data = data[:int(n*0.9)]
+print("n : ",n)
+train_data = data[:int(n*0.9)] #将前90%化为训练集，后10%为验证集
 val_data = data[int(n*0.9):]
 
 # encode both to integers
@@ -66,3 +69,4 @@ with open(os.path.join(os.path.dirname(__file__), 'meta.pkl'), 'wb') as f:
 # vocab size: 65
 # train has 1003854 tokens
 # val has 111540 tokens
+
